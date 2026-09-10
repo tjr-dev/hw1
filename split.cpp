794327e2f6f96732fe53c8b975f77941da24f560
Tenor Reilly <tjreilly@usc.edu>
2026-09-10 13:29:46 -0700

    testing first commit

--- split.cpp
+++ split.cpp
@@ -16,8 +16,7 @@ the function below should be the only one in this file.
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+  
 }
 
 /* If you needed a helper function, write it here */
